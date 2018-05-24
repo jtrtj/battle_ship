bdd3bf96a91b763bb9896049fdd135df8b31d219
JohnRoemer <jt.r@live.com>
2018-05-24 17:18:41 -0600

    refactorsss

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,15 @@
+require './lib/space.rb'
+
 class Board
   attr_reader :spaces
   def initialize
-    @spaces =
-    [[Space.new('00'), Space.new('01'), Space.new('02'), Space.new('03')],
-    [Space.new('10'), Space.new('11'), Space.new('12'), Space.new('13')],
-    [Space.new('20'), Space.new('21'), Space.new('22'), Space.new('23')],
-    [Space.new('30'), Space.new('31'), Space.new('32'), Space.new('33')]]
+    @spaces = Board.of_size(4)
+  end
+
+  def self.of_size(board_size)
+    (0..3).map do |space|
+      [Space.new(space.to_s+'0'), Space.new(space.to_s+'1'), Space.new(space.to_s+'2'), Space.new(space.to_s+'3')]
+    end
   end
 
   def print_board
@@ -20,3 +24,9 @@ class Board
   end
 
 end
+
+
+[[Space.new('00'), Space.new('01'), Space.new('02'), Space.new('03')],
+[Space.new('10'), Space.new('11'), Space.new('12'), Space.new('13')],
+[Space.new('20'), Space.new('21'), Space.new('22'), Space.new('23')],
+[Space.new('30'), Space.new('31'), Space.new('32'), Space.new('33')]]
